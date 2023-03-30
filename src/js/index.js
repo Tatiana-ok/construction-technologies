8eb333f37a160a39c1bcd8e83b8005028a9921b1
Tetiana Pomazunovska <my.mail.tetiana@gmail.com>
2023-03-30 09:52:28 +0300

    Добавляет parcel

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import './styles.scss';
+import templateFunction from './template.hbs';
+
+//Objects
+
+const pointEntry = document.querySelector('.pointEntry');
+const objectsList = document.querySelector('.objects-list');
